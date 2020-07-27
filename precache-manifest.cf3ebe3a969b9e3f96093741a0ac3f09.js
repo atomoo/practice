6519069713ef1b13a4fd7aa3f2232ba387ab992a
bot <actions@github.com>
2020-07-27 10:05:26 +0000

    auto deploy

---
+++ precache-manifest.cf3ebe3a969b9e3f96093741a0ac3f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f546ef667aca9b0ec9a89bdc339eb7cd",
+    "url": "/practice/index.html"
+  },
+  {
+    "revision": "b4b73b2a3f56d962d567",
+    "url": "/practice/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "abeed7fa3907301674f8",
+    "url": "/practice/static/css/main.014a8f4d.chunk.css"
+  },
+  {
+    "revision": "b4b73b2a3f56d962d567",
+    "url": "/practice/static/js/2.3916c12b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/practice/static/js/2.3916c12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abeed7fa3907301674f8",
+    "url": "/practice/static/js/main.3ba6b159.chunk.js"
+  },
+  {
+    "revision": "bda164b9984a57f9cf68",
+    "url": "/practice/static/js/runtime-main.4fe44ec3.js"
+  }
+]);
